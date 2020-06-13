78c77b9c5076ee1efba0f41205bdab2fcc44ff52
shailsoni44 <shailsoni44@gmail.com>
2020-06-13 18:27:51 -0500

    Auth Project First Commit

---
+++ Authproject/users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','email']
+
+class CustomUserChangeForm(UserChangeForm):
+    model=User
+    fields=UserChangeForm.Meta.fields
